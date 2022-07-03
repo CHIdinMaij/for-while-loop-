f2a7cca7a3a98a1bcb2e62d4b911b4d0b1cf935c
chidinma <cijeoma214@gmail.com>
2022-07-03 23:03:46 +0100

    Add files via upload

---
+++ loop_js.js
@@ -0,0 +1,18 @@
+// we are writing a code that checks if a num is less than 100
+//firstly we intialize to zero
+//secondly the condition a  numbeer to the varible
+// statement to keep adding until is 100 it terminates
+//while loop to increment
+var num=0;
+while(num<100)
+{
+num+=1;
+ console.log(num);
+
+}
+// we used for loop to decrement
+ 
+for (let num1=200;
+    num1>=100;num1--){
+        console.log(num1);
+    }
